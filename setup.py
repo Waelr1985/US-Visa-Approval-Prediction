86e41526a3634740fa2d8724876b3e1d9f79dad8
Wael Rahhal <waelr1985@gmail.com>
2024-05-07 03:20:42 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Wael",
+    author_email="waelr1985@gmail.com",
+    packages=find_packages()
+)
