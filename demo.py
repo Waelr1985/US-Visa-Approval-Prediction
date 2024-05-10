73bb5a5866ba857985f970956ed71f7bde97a75b
Wael Rahhal <waelr1985@gmail.com>
2024-05-11 01:26:11 +0300

    exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+
 
 logging.info('welcome')
+
+try:
+    a = 1 / "10"
+except Exception as e:
+    raise USvisaException(e, sys) from e
