81b707909f4854e384f0ec6ed77268a4ca1eb253
Wael Rahhal <waelr1985@gmail.com>
2024-05-12 05:41:50 +0300

    updated

--- demo.py
+++ demo.py
@@ -1,12 +1,3 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.constants import *
 
-
-logging.info('welcome')
-
-try:
-    a = 1 / "10"
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys) from e
+print(COLLECTION_NAME)
